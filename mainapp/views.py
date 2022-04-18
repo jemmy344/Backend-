deaf0c097dc773e8ededc29b82086911b949bc36
jemmy344 <101700073+jemmy344@users.noreply.github.com>
2022-04-18 20:40:05 +0100

    updated logic for privacy policy

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,4 @@
 from ast import Return
-from email.policy import default
 from django import http
 from django.http import JsonResponse
 from django.shortcuts import render, HttpResponse
@@ -7,6 +6,8 @@ from urllib.parse import urlparse
 import json
 from .models import RegisteredSites
 import requests
+import re
+from bs4 import BeautifulSoup
 
 from django.views.decorators.csrf import csrf_exempt
 
@@ -20,30 +21,52 @@ def cookieChecker(url):
   if cookies: 
     return True
   return False
+
+def checker(url): 
+  source = requests.get(url).text
+  soup = BeautifulSoup(source, "lxml")
+
+  search = soup.findAll(text=re.compile('cookies'))
+
+  for i in search:
+    soup = i.parent.text
+    if ("accept" in soup or "Accept" in soup or "Allow" in soup or "allow" in soup):
+      return True
+
+
+  search1 = soup.findAll("a")
+  for i in search1:
+    link = i.text
+    if ("privacy" in link or "policy" in link or "Privacy" in link or "Policy" in link) : 
+      return True
+    
+  #print(search)
+  return False 
   
   
 
 
 @csrf_exempt
 def checkurl(requests):
-    if requests.method != 'POST':
-        return HttpResponse('Invalid method', 403)
-    data = json.loads(requests.body)
-    url = data.get('url')
-    uri = urlparse(url)
-
-    if not (uri.scheme or uri.netloc):
-        return JsonResponse({'code':500, 'status':'error', 'message': 'invalid url. url must contain url scheme and domain'})
+	if requests.method != 'POST':
+		return HttpResponse('Invalid method', 403)
+	print(requests.POST.get('name'))
+	data = json.loads(requests.body)
+	url = data.get('url')
+	uri = urlparse(url)
+	
+	if not (uri.scheme or uri.netloc):
+		return JsonResponse({'code':500, 'status':'error', 'message': 'invalid url. url must contain url scheme and domain'})
     
-    obj, created = RegisteredSites.objects.get_or_create(
-        url = uri.netloc,
-        defaults = {
-            # 'url':uri.netloc,
-            'has_cookie':cookieChecker(url)
-        }
-
-    )
-    
-
-    response = JsonResponse({'code':200,'status':'success','has_cookie':obj.has_cookie})
-    return response
+	# obj, created = RegisteredSites.objects.get_or_create(
+	# 	url = uri.netloc,
+	# 	defaults = {
+	# 		# 'url':uri.netloc,
+	# 		'has_cookie':cookieChecker(url)
+	# 	}
+	
+	# )
+	
+	checker(url)
+	response = JsonResponse({'code':200,'status':'success','has_cookie':cookieChecker(url), 'privacy_policy': cookieChecker(url) and checker(url) })
+	return response
